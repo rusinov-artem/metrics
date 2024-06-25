b37e2f71b65d8faa647dfc4031e350141754e371
Artem Rusinov <a.rusinov@lamoda.ru>
2024-06-25 22:30:44 +0300

    fix nesting middlewares

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -5,9 +5,10 @@ import "net/http"
 type Middleware func(http.Handler) http.Handler
 
 func (t *Middleware) Wrap(m Middleware) Middleware {
+	parent := *t
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			m(h).ServeHTTP(w, r)
+			m((parent)(h)).ServeHTTP(w, r)
 		})
 	}
 }
