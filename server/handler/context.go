6bd00f34bbb3e3dc862c8caf1024317ddd38aaee
Rusinov Artem <rusinov.dev@gmail.com>
2024-10-05 13:42:56 +0300

    iter18. Добавил документацию

--- server/handler/context.go
+++ server/handler/context.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// context Используется для создания контекста во всех хендлерах
 func (h *Handler) context(ctx context.Context) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(ctx, 5*time.Second)
 }
